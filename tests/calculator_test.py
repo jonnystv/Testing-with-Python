4d08488aa9dcadca336390a44f9e17a8b5967d4b
jonnystv <jonathan@darefoundation.com>
2020-11-24 23:30:45 +0000

    homework MVP complete

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -5,4 +5,21 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(5, add(2, 3))
-    pass
+    
+    
+    # def test_add(self):
+    #     expected = 5
+    #     actual = add(2, 3)
+    #     self.assertEqual(expected, actual)
+
+
+    def test_subtract(self):
+        self.assertEqual(3, subtract(10, 7))
+
+
+    def test_divide(self):
+        self.assertEqual(10, divide(30, 3))
+
+
+    def test_multiply(self):
+        self.assertEqual(27, multiply(3, 9))
